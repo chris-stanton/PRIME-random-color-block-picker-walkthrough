0b6794239762375373e65b5368866e1e9615b04a
Chris Stanton <chrisstanton@Chriss-MacBook-Pro.local>
2017-02-15 10:28:08 -0600

    added andom color

--- client.js
+++ client.js
@@ -6,11 +6,19 @@ $(document).ready(function() {
   var colorArray = ["AliceBlue", "AntiqueWhite", "Aqua", "Aquamarine"];
 //loops through all of the colors and applies to the empty <div>
   for (var i = 0; i < colorArray.length; i++) {
-    colorArray[i]
-  }
-
   var newColorBlock = $("<div>");
   newColorBlock.css("background-color", colorArray[i]);
   newColorBlock.addClass('colorBlocks');
   $('#colorBlockContainer').append(newColorBlock);
+  }
+
+//select a random color and add it to the DOM
+var randomNUmberSelected = randomNumber(0, colorArray.length-1); //randomly selects a number from 0-4
+var randomColor = colorArray[randomNUmberSelected];
+$('#userColorPrompt').text(randomColor);
+
+
+function randomNumber(min, max){
+    return Math.floor(Math.random() * (1 + max - min) + min);
+}
 });
