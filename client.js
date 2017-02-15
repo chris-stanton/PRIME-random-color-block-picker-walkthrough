f442b6f6e94ec198a5c6248de3dc11931062ece7
Chris Stanton <chrisstanton@Chriss-MacBook-Pro.local>
2017-02-15 10:09:31 -0600

    add 1st portion of code

--- client.js
+++ client.js
@@ -1,4 +1,16 @@
 //Use only with jQuery
 $(document).ready(function() {
+//log to say were are ready
+  console.log("jquery is ready to go");
+//creates blocks, applies css and appends to the DOM
+  var colorArray = ["AliceBlue", "AntiqueWhite", "Aqua", "Aquamarine"];
+//loops through all of the colors and applies to the empty <div>
+  for (var i = 0; i < colorArray.length; i++) {
+    colorArray[i]
+  }
 
+  var newColorBlock = $("<div>");
+  newColorBlock.css("background-color", colorArray[i]);
+  newColorBlock.addClass('colorBlocks');
+  $('#colorBlockContainer').append(newColorBlock);
 });
